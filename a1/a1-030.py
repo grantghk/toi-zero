b5cc695f7828a77f0c3f6219046f042d51f1a521
grantghk <grantghk@gmail.com>
2025-05-15 23:16:25 +0700

    Added a1-030

---
+++ a1/a1-030.py
@@ -0,0 +1,19 @@
+n = int(input())
+numbers = list(map(int, input().split()))
+
+if len(numbers) != 2 * n:
+    exit()
+
+greater_values = []
+
+for i in range(0, 2 * n, 2):
+    a = numbers[i]
+    b = numbers[i + 1]
+    greater_values.append(max(a, b))
+
+if len(greater_values) == 1:
+    print(greater_values[0])
+else:
+    equation = " + ".join(map(str, greater_values))
+    total = sum(greater_values)
+    print(f"{equation} = {total}")
